f368295818cb74a9d5ff61a43ab43a31f1b7e76c
olatunde <lawalolatunde@ymail.com>
2022-04-13 05:51:45 -0700

    newer

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""getting data from an api
+"""getting data from an api and export in JSON
 """
 
+import json
 import requests
 from sys import argv
 
 if __name__ == '__main__':
     api_url = "https://jsonplaceholder.typicode.com/users"
     users = requests.get(api_url)
-    EMPLOYEE_NAME = ""
     for i in users.json():
         if i.get("id") == int(argv[1]):
-            EMPLOYEE_NAME = i.get("name")
+            USERNAME = i.get("username")
             break
-    NUMBER_OF_DONE_TASKS = 0
-    TOTAL_NUMBER_OF_TASKS = 0
-    TASK_TITLE = []
+    TASK_STATUS_TITLE = []
 
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
+    todos_url = "http://jsonplaceholder.typicode.com/todos"
     todos = requests.get(todos_url)
 
-    for task in todos.json():
-        if task.get("userId") == int(argv[1]):
-            TOTAL_NUMBER_OF_TASKS += 1
-            if task.get("completed") is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE.append(task.get("title"))
-    print("Employee {} is done with tasks({}/{}):".format(
-        EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
-    for task in TASK_TITLE:
-        print("\t {}".format(task))
+    for t in todos.json():
+        if t.get("userId") == int(argv[1]):
+            TASK_STATUS_TITLE.append((t.get('completed'), t.get('title')))
+
+    """export to json"""
+    t = []
+    for task in TASK_STATUS_TITLE:
+        t.append({"task": task[1], "completed": task[0], "username": USERNAME})
+    data = {str(argv[1]): t}
+    filename = "{}.json".format(argv[1])
+    with open(filename, "w") as f:
+        json.dump(data, f)
