a4e7da7b585614222cb1cbf50c5e2ce61534b54f
hollaberry <69259629+hollaberry@users.noreply.github.com>
2022-09-24 12:11:37 -0700

    second

--- react_learning/facerecognitionbrain/src/components/SignIn/SignIn.js
+++ react_learning/facerecognitionbrain/src/components/SignIn/SignIn.js
@@ -32,6 +32,7 @@ class SignIn extends React.Component {
                 this.props.loadUser(user);
                 this.props.onRouteChange('home');
             }
+           
         })
         
     }
